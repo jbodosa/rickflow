337f7d0746137b20b0720c1c3ec30e5d5c80dc98
Andreas Krämer <kraemer.research@gmail.com>
2018-12-04 06:25:53 +0100

    ...

--- rflow/templates/sdyn_pascal.sh
+++ rflow/templates/sdyn_pascal.sh
@@ -11,4 +11,4 @@ module load cuda/9.2 # load the appropriate cuda module (e.g. 'cuda/9' for the c
 sleep 10
 
 # run simulation and resubmit script
-./dyn.py && rickflow submit sdyn_pascal.sh && sleep 60
+./dyn.py && rflow submit sdyn_pascal.sh && sleep 60
