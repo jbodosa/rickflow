0c82a29cb69d7b92c085b1a82e77f1461304c2db
Andreas Krämer <kraemer.research@gmail.com>
2019-02-08 17:15:57 -0500

    Added observables to __init__

--- rflow/__init__.py
+++ rflow/__init__.py
@@ -16,4 +16,4 @@ from rflow.exceptions import *
 from rflow.integrators import NonequilibriumLangevinIntegrator
 from rflow.rickflow import *
 from rflow.trajectory import *
-from rflow.observables import TimeSeries, AreaPerLipid
+from rflow.observables import TimeSeries, AreaPerLipid, Coordinates, BoxSize
