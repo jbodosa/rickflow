9fd8901de4e7e8a7ac70d9ced4ee23c501a19cfc
Andreas Krämer <kraemer.research@gmail.com>
2019-06-07 09:56:54 -0400

    more meaningful test for increment_using_multiindices

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -5,5 +5,5 @@ from rflow.utility import increment_using_multiindices
 
 def test_increment():
     a = np.zeros((2,2), dtype=int)
-    res = increment_using_multiindices(a, np.array([[0,0], [1,1]], dtype=int))
-    assert np.array_equal(res, np.array([[1,0], [0,1]], dtype=int))
+    res = increment_using_multiindices(a, np.array([[0,0], [1,1], [1,1]], dtype=int))
+    assert np.array_equal(res, np.array([[1,0], [0,2]], dtype=int))
