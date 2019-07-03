117c9f1dcf78ece4273cfac4ffeae8037085e5f8
Andreas Krämer <kraemer.research@gmail.com>
2019-07-03 16:00:32 +0000

    Update dyn.py

--- examples/start_simulation/dyn.py
+++ examples/start_simulation/dyn.py
@@ -5,10 +5,8 @@ import os
 import numpy as np
 
 import simtk.unit as u
-from simtk.openmm.app import CharmmPsfFile, CharmmCrdFile, CharmmParameterSet, PME
-from simtk.openmm.app import HBonds, Simulation, DCDReporter, StateDataReporter
-from simtk.openmm import MonteCarloAnisotropicBarostat, LangevinIntegrator
-from rflow.integrators import NoseHooverChainVelocityVerletIntegrator
+from simtk.openmm.app import PME
+from simtk.openmm import LangevinIntegrator
 
 from rflow import RickFlow
 
@@ -36,15 +34,17 @@ temperature = 310.0 * u.kelvin
 # integrator (The Nose-Hoover integrator in openmm does is currently not using the right
 # number of degrees of freedom. The implementation in nosehoover.py provides a short-term fix,
 # which requires the system to be passed to the constructor)
+# from rflow.integrators import NoseHooverChainVelocityVerletIntegrator
 # integrator = NoseHooverChainVelocityVerletIntegrator(
 #        workflow.system, temperature, 50.0 / u.picosecond, 1.0 * u.femtosecond,
 #        chain_length=3, num_mts=3, num_yoshidasuzuki=3
 #)
 integrator = LangevinIntegrator(temperature, 1.0 / u.picosecond, 1.0 * u.femtosecond)
-#barostat = MonteCarloAnisotropicBarostat(
-#    u.Quantity(value=np.array([0.0, 0.0, 1.0]), unit=u.atmosphere), temperature,
-#    False, False, True
-#)
+# from simtk.openmm import MonteCarloAnisotropicBarostat
+# barostat = MonteCarloAnisotropicBarostat(
+#     u.Quantity(value=np.array([0.0, 0.0, 1.0]), unit=u.atmosphere), temperature,
+#     False, False, True
+# )
 
 workflow.prepareSimulation(integrator)#, barostat)
 
